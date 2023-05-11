455ba4084d512f28626e34e3ef6dab8c90c8766a
Eoghan Gibbons <eoghanfrommayo@live.com>
2023-05-11 17:22:45 +0200

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name           Rock Werchter Bands timetable helper
+// @description    Provides functionality like that of the RW app
+// @author         Eoghan Gibbons
+// @match        https://www.rockwerchter.be/*
+// @require	   https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @version        1.0
+// ==/UserScript==
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+addGlobalStyle(".selected-item-act {background-color: orange !important; color: white !important;}");
+
+$('a').on("click", function(e) {
+    e.preventDefault();
+    let target = e.currentTarget;
+
+    if (target.classList.contains("selected-item-act")) {
+        target.classList.remove("selected-item-act");
+    } else {
+        target.classList.add("selected-item-act");
+    }
+});
